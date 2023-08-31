cc61a4d85f259b396b085f3044f7ec98987fded7
Rasalahmad <rasalahmadsit@gmail.com>
2023-08-31 11:39:52 +0600

    wip

--- routes/order.js
+++ routes/order.js
@@ -191,4 +191,32 @@ router.post("/:id", async (req, res) => {
   }
 });
 
+//GET MONTHLY INCOME STATS
+
+router.get("/income-stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await Order.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          total_amount: "$total_amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total_amount: { $sum: "$total_amount" },
+        },
+      },
+    ]);
+    res.status(200).json({ success: true, data });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err });
+  }
+});
+
 module.exports = router;
