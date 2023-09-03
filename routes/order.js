7bebdb754015179bf019bf4452bf3a9be275c9d9
Rasalahmad <rasalahmadsit@gmail.com>
2023-09-03 20:33:47 +0600

    wip

--- routes/order.js
+++ routes/order.js
@@ -24,7 +24,7 @@ router.post("/cash-on-delivery", async (req, res) => {
     cus_name: name,
     cus_email: email,
     cus_add1: address,
-    cus_add2: city,
+    cus_city: city,
     cus_postcode: postcode,
     cus_phone: phone,
     cus_fax: phone,
@@ -34,9 +34,11 @@ router.post("/cash-on-delivery", async (req, res) => {
     data,
     products,
     paymentStatus: "Pending",
+    shippingStatus: "Pending",
     transaction_Id,
     user: _id,
     total_amount,
+    shipping_method: "Courier",
   });
 
   try {
@@ -63,7 +65,7 @@ router.post("/payment", async (req, res) => {
     total_amount: total_amount,
     currency: "BDT",
     product_img: products[0]?.img,
-    tran_id: transaction_Id,
+    transaction_Id: transaction_Id,
     success_url: `${process.env.StoreRoute}/payment/success/${transaction_Id}`,
     fail_url: `${process.env.StoreRoute}/payment/fail/${transaction_Id}`,
     cancel_url: `${process.env.StoreRoute}/cancel/${transaction_Id}`,
@@ -108,6 +110,7 @@ router.post("/payment", async (req, res) => {
         data,
         products,
         paymentStatus: "Pending",
+        shippingStatus: "Pending",
         transaction_Id,
         user: _id,
         total_amount,
