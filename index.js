3bd622452960abd1f6f101634d41207a930d5942
Rasalahmad <rasalahmadsit@gmail.com>
2024-05-14 13:12:18 +0600

    cors issue solve

--- index.js
+++ index.js
@@ -21,10 +21,8 @@ mongoose
 const whitelist = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://api.rockmetaltshirt.com",
   "https://rockmetaltshirt.com",
   "https://admin.rockmetaltshirt.com",
-  "https://rock-metal-backend-6htsx5og2-chttravels-projects.vercel.app",
 ];
 const corsOptions = {
   origin: function (origin, callback) {
